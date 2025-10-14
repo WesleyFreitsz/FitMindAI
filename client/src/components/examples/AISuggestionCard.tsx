5a7170d5b606c5a1b2b61a5117ab77fae50a6160
Wesley Vinicius <128614188+WesleyFreitsz@users.noreply.github.com>
2025-10-14 11:57:28 -0300

    delete unused archives

--- client/src/components/examples/AISuggestionCard.tsx
+++
@@ -1,9 +0,0 @@
-import AISuggestionCard from '../AISuggestionCard';
-
-export default function AISuggestionCardExample() {
-  return (
-    <div className="w-full max-w-md p-4">
-      <AISuggestionCard message="Faltam 30g de proteína para atingir sua meta diária. Que tal adicionar um shake proteico?" />
-    </div>
-  );
-}
--- client/src/components/examples/QuickAddFood.tsx
+++
@@ -1,9 +0,0 @@
-import QuickAddFood from '../QuickAddFood';
-
-export default function QuickAddFoodExample() {
-  return (
-    <div className="w-full max-w-md p-4">
-      <QuickAddFood onSubmit={(text) => console.log('Food submitted:', text)} />
-    </div>
-  );
-}
